93ac362fcbc5c1de706ffca53451aba11f546fbd
Surjendu kar <rahulgotp9988@gmai.com>
2025-01-01 19:19:54 +0530

    Fea: redirecting to blog page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/blog");
+  }, [router]);
+
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <div className="animate-pulse text-lg">Home page</div>
+      <div className="animate-pulse text-lg">Redirecting....</div>
     </div>
   );
 }
